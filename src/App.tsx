db4a183289106d6eab4b52ba3283dedc9817c569
Annabel Wang <annabew3@uci.edu>
2024-05-25 13:30:20 -0700

    created LookingForForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 import Home from "./pages/Home";
+import LookingForForm from "./components/LookingForForm";
 
 function App() {
   //const [count, setCount] = useState(0);
 
   return (
     <div>
-      <Home />
+      <LookingForForm />
     </div>
   );
 }
