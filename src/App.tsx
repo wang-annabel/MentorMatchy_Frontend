4139cc5761381c2d22b793752f98811d6226b998
Grace Wang <gwang7@uci.edu>
2024-05-25 21:39:30 -0700

    added components to Matches and Profile pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./components/navbar/index";
 import LoginPage from "./components/pages/login";
-import MatchesPage from "./components/pages/login";
-import ProfilePage from "./components/pages/login";
+import MatchesPage from "./components/pages/matches";
+import ProfilePage from "./components/pages/profile";
 import Navbar from "./components/navbar/NavbarElements";
 import AccountCreation from "./pages/AccountCreation";
 import AccountCreationBasicInfo from "./pages/AccountCreation_BasicInfo";
@@ -11,18 +11,20 @@ import WorkStyle from "./pages/AccountCreation_WorkStyle";
 
 const App = () => {
   return (
+    <>
     <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/matches" element={<ProfilePage />} />
-        <Route path="/profile" element={<MatchesPage />} />
+        <Route path="/matches" element={<MatchesPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
         <Route path="/accountCreation" element={<AccountCreation />} />
         <Route path="/basicInfo" element={<AccountCreationBasicInfo />} />
         <Route path="/workStyle" element={<WorkStyle />} />
       </Routes>
     </Router>
+    </>
   );
 };
 
