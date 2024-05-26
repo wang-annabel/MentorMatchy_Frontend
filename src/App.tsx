717e27ac7575384eb6daa3ec13550c737f101920
Grace Wang <gwang7@uci.edu>
2024-05-25 17:26:10 -0700

    Components of mentee profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import ProfilePhoto from './components/ProfilePhoto';
+import UserInfo from './components/UserInfo';
+import MenteeMatched from './components/Mentee_MatchedWith';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <Container>
+            <Row>
+                <Col><ProfilePhoto name='Jane Doe' pronouns='she/her' email='jdoe@gmail.com'></ProfilePhoto></Col>
+                <Col><UserInfo about='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in turpis eu leo feugiat gravida vel eu erat. Maecenas lacinia aliquet felis vitae sagittis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec vestibulum velit ipsum, ac rutrum sapien aliquet sed. Etiam malesuada sapien vel.' interests='Lorem, ipsum, dolor, sit, amet' industry='Film/Media'></UserInfo></Col>           
+            </Row>
+            <div><MenteeMatched name="Greta Gerwig"></MenteeMatched></div>
+        </Container>
+    );
 }
 
-export default App
+export default App;
